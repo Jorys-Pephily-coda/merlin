bb9ab13db91492a016fef8d0fd907c5bf65dc6a0
Jorys-Pephily-coda <jorys.pephily@coda-student.school>
2025-09-23 15:40:55 +0200

    main.php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'jorys/merlin',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '9cf75eca112b16a6665e0b3330de371b178310f7',
+        'reference' => 'cf48c4210c6840ea520c7a641d0ee5a4d86e94a6',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'jorys/merlin' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '9cf75eca112b16a6665e0b3330de371b178310f7',
+            'reference' => 'cf48c4210c6840ea520c7a641d0ee5a4d86e94a6',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
