96c05c204358580929a06c69d55473ebb05ae4a6
Greed <181872123+LeVictorSSB@users.noreply.github.com>
2025-09-24 14:28:23 +0200

    mob pont

--- Escalibur/Maps/Pont.php
+++ Escalibur/Maps/Pont.php
@@ -2,14 +2,15 @@
 
 namespace Merlin\Escalibur\Maps;
 
-use Merlin\Component\Map\Blueprint;
 use Merlin\Game\Position\Position;
+use Merlin\Component\Map\Blueprint;
+use Merlin\Escalibur\Monsters\Minotoror;
 
 class Pont extends Blueprint {
 
     public function npcs(): array { return []; }
     public function items(): array { return []; }
-    public function monsters(): array { return []; }
+    public function monsters(): array { return [new Minotoror()]; }
     public function name(): string { return "Pont du destin"; }
     public function description(): string { return "Un passage dangereux qu'il faut franchir en prouvant sa valeur."; }
     public function position(): Position { return new Position(2,2); }
