100e7ae725e05f35773c8f6025614e699e32c324
Greed <181872123+LeVictorSSB@users.noreply.github.com>
2025-09-24 15:03:37 +0200

    FIX GOUFFRE

--- src/Component/Map/Map.php
+++ src/Component/Map/Map.php
@@ -174,7 +174,7 @@ class Map extends AbstractComponent {
         } elseif ($this->current_position->isSame(new Position(0, 3))) {
             $pp->writeLn('Vous tombez dans le gouffre!', null, 'red');
             $pp->writeLn('Profitez de votre chute', null, 'red');
-            $this->container->getConsole()->run('fight Gouffre');
+            $this->container->dispatcher()->dispatch('character.fight', ['monster' => 'Gouffre']);
         }
     }
 
