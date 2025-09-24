e0407eb273e5ca07877ee3e085f4ebd224e2a74f
Greed <181872123+LeVictorSSB@users.noreply.github.com>
2025-09-24 14:58:15 +0200

    mob foret

--- Escalibur/Maps/Foret.php
+++ Escalibur/Maps/Foret.php
@@ -3,13 +3,14 @@
 namespace Merlin\Escalibur\Maps;
 
 use Merlin\Component\Map\Blueprint;
+use Merlin\Escalibur\Monsters\Piou;
 use Merlin\Game\Position\Position;
 
 class Foret extends Blueprint {
 
     public function npcs(): array { return []; }
     public function items(): array { return []; }
-    public function monsters(): array { return []; }
+    public function monsters(): array { return [new Piou()]; }
     public function name(): string { return "Forêt de Brocéliande"; }
     public function description(): string { return "Une forêt magique, mystérieuse, peuplée de fées et de druides."; }
     public function position(): Position { return new Position(1,1); }
