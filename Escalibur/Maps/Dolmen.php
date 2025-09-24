16138aaceaf98fd6061c29e642931e46d7ded8ad
Greed <181872123+LeVictorSSB@users.noreply.github.com>
2025-09-24 11:32:13 +0200

    dolmen

--- Escalibur/Maps/Dolmen.php
+++ Escalibur/Maps/Dolmen.php
@@ -11,6 +11,6 @@ class Dolmen extends Blueprint {
     public function items(): array { return []; }
     public function monsters(): array { return []; }
     public function name(): string { return "Dolmen de l’Éternité"; }
-    public function description(): string { return "1 chateau, 3 maisons"; }
+    public function description(): string { return "Monument mégalithique, utilisé comme autel dans les cultes druidiques. il est eternel."; }
     public function position(): Position { return new Position(1,2); }
 }
